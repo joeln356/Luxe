c3a92e6f324ee3fa220705865e9fa43a138b50b5
Joel <joelndala356@gmail.com>
2026-07-04 13:28:51 +0100

    f: responsive design

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,7 @@
 // BTN MENU
 const btn_menu = document.getElementById('Menu')
 const menu = document.getElementById('nav')
-
+const agendar_btn = document.getElementById('agenda')
 btn_menu.addEventListener('click', ()=>{
     btn_menu.classList.toggle('ativar_menu_burger')
 })
